08ca42ad112f32b96f10d3809a9cab59352c2212
Fuzail Khan <ifuzail1234@gmail.com>
2023-08-20 20:32:18 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import SpecialOffer from './SpecialOffer'
+
+export{
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Services,
+    Subscribe,
+    SuperQuality,
+    SpecialOffer
+}
